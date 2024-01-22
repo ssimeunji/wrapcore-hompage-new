d7458b10534b7a696bd9e3335ddbde61634522da
ssimeunji <ssimeunji@wrapcore.com>
2024-01-22 15:42:42 +0900

    css fix

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,30 +1,29 @@
-  const wrapper = document.querySelector('.slide_list');
-  const slides = document.querySelectorAll('.slide_item');
-  let currentSlide = 0;
+const wrapper = document.querySelector('.slide_list');
+const slides = document.querySelectorAll('.slide_item');
+let currentSlide = 0;
 
-  function showSlide(index) {
-    const slideWidth = slides[0].clientWidth;
-    wrapper.style.transition = 'transform 0.5s ease-in-out';
-    wrapper.style.transform = `translateX(-${index * slideWidth}px)`;
+function showSlide(index) {
+  const slideWidth = slides[0].clientWidth;
+  wrapper.style.transition = 'transform 0.5s ease-in-out';
+  wrapper.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+function nextSlide() {
+  if (currentSlide < slides.length - 1) {
+    currentSlide++;
+  } else {
+    currentSlide = 0;
   }
+  showSlide(currentSlide);
+}
 
-  function nextSlide() {
-    if (currentSlide < slides.length - 1) {
-      currentSlide++;
-    } else {
-      currentSlide = 0;
-    }
-    showSlide(currentSlide);
-  }
+// 자동으로 슬라이드 변경 (4초마다)
+setInterval(nextSlide, 4000);
 
-  // 자동으로 슬라이드 변경 (4초마다)
-  setInterval(nextSlide, 4000);
-  
-  const render = () => {
-    showSlide();
-    nextSlide();
-    setInterval();
-
+const render = () => {
+  showSlide();
+  nextSlide();
+  setInterval();
 };
 
 render();
